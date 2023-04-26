e32ca21eaa4d1c752d8ae4a31073ca3f6f92d05d
luteoos <mateusz.lutecki.it@gmail.com>
2023-04-26 17:57:24 +0200

    run ktlint

--- core/src/commonMain/kotlin/dev/luteoos/scrumbet/core/KState.kt
+++ core/src/commonMain/kotlin/dev/luteoos/scrumbet/core/KState.kt
@@ -1,7 +1,5 @@
 package dev.luteoos.scrumbet.core
 
-import dev.luteoos.scrumbet.data.entity.AppException
-
 /**
  * Based on [moko - ResourceState](https://github.com/icerockdev/moko-mvvm/blob/master/mvvm-state/src/commonMain/kotlin/dev/icerock/moko/mvvm/ResourceState.kt)
  */
@@ -12,4 +10,3 @@ sealed interface KState<out T, out E> {
     data class Success<T>(val value: T) : KState<T, Nothing>
     data class Error<E>(val error: Throwable) : KState<Nothing, E>
 }
-
