fa0786488634f7f9d2eef706c42f992aaf01df4e
Luteoos <mateusz.lutecki.it@gmail.com>
2023-05-08 17:56:40 +0200

    added logging to `ktor`

--- domain/src/commonMain/kotlin/dev/luteoos/scrumbet/domain/util/HttpClientProvider.kt
+++ domain/src/commonMain/kotlin/dev/luteoos/scrumbet/domain/util/HttpClientProvider.kt
@@ -2,13 +2,23 @@ package dev.luteoos.scrumbet.domain.util
 
 import io.ktor.client.HttpClient
 import io.ktor.client.HttpClientConfig
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.websocket.WebSockets
 import io.ktor.client.request.headers
 import io.ktor.client.request.request
+import io.ktor.http.HttpHeaders
 import io.ktor.serialization.kotlinx.KotlinxWebsocketSerializationConverter
 import kotlinx.serialization.json.Json
 
 fun getHttpClient() = getPlatformHttpClient {
+    install(Logging){
+        logger = Logger.DEFAULT
+        level = LogLevel.ALL
+        sanitizeHeader { header -> header == HttpHeaders.Authorization }
+    }
     install(WebSockets){
         pingInterval = 5_000
         contentConverter = KotlinxWebsocketSerializationConverter(Json)
