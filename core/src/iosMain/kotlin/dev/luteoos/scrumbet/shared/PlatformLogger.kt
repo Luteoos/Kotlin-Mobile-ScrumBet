af89973b97e062493794252173a7ff985212a656
luteoos <mateusz.lutecki.it@gmail.com>
2023-06-06 21:02:57 +0200

    - fixed `crashlytics` on ios - `PlatformLogger.kt` using `NSLog`

--- core/src/iosMain/kotlin/dev/luteoos/scrumbet/shared/PlatformLogger.kt
+++ core/src/iosMain/kotlin/dev/luteoos/scrumbet/shared/PlatformLogger.kt
@@ -1,6 +1,13 @@
 package dev.luteoos.scrumbet.shared
 
+import platform.Foundation.NSLog
+
 actual class PlatformLogger {
+
+    //TODO rethink
     actual fun log(tag: Level, message: String, e: Exception?) {
+        NSLog("${tag.tag}/ $message")
+        if(e != null)
+            NSLog("${tag.tag}/ $e")
     }
 }
--- iosApp/Pods/Target Support Files/Pods-iosApp/Pods-iosApp-frameworks.sh
+++ iosApp/Pods/Target Support Files/Pods-iosApp/Pods-iosApp-frameworks.sh
@@ -177,19 +177,29 @@ code_sign_if_enabled() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/FirebaseCore/FirebaseCore.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FirebaseCoreExtension/FirebaseCoreExtension.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FirebaseCoreInternal/FirebaseCoreInternal.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FirebaseCrashlytics/FirebaseCrashlytics.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FirebaseInstallations/FirebaseInstallations.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FirebaseSessions/FirebaseSessions.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GoogleDataTransport/GoogleDataTransport.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PromisesObjC/FBLPromises.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PromisesSwift/Promises.framework"
   install_framework "${PODS_ROOT}/../../core/build/cocoapods/framework/core.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/FirebaseCore/FirebaseCore.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FirebaseCoreExtension/FirebaseCoreExtension.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FirebaseCoreInternal/FirebaseCoreInternal.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FirebaseCrashlytics/FirebaseCrashlytics.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FirebaseInstallations/FirebaseInstallations.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FirebaseSessions/FirebaseSessions.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GoogleDataTransport/GoogleDataTransport.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PromisesObjC/FBLPromises.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PromisesSwift/Promises.framework"
   install_framework "${PODS_ROOT}/../../core/build/cocoapods/framework/core.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
