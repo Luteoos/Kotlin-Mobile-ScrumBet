6588e4680e9bc1644182faea81f25ed44925ed6d
luteoos <mateusz.lutecki.it@gmail.com>
2023-06-12 14:57:08 +0200

    - added `*.xconfig` to project - added `API_BASE_URL` to `.plist` - added `AuthObject.swift` , `AuthUiState.swift`

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -7,14 +7,19 @@ struct iOSApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     //    way to do DI via @EnvironmentObject
-    let authObject : MockObservableObject
+    let authObject : AuthObject
 
     init(){
         // IOSApp is invoked also for PreviewProviders
         KoinKt.doInitKoin()
         // must be after .doInitKoin due to underlying inject()
         //authObject = ObservableObject(controller: KController())
-        authObject = MockObservableObject()
+        authObject = AuthObject(controller:
+//                                    MockAuthControllerInterface())
+                                    AuthController(
+                                       preferences: nil,
+                                       serverRepository: nil,
+                                       applicationVersion: nil) )
     }
 
 	var body: some Scene {
@@ -29,7 +34,7 @@ struct iOSApp: App {
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-      print("dev.luteoos.template is starting up. ApplicationDelegate didFinishLaunchingWithOptions.")
+      print("dev.luteoos.scrumbet is starting up. ApplicationDelegate didFinishLaunchingWithOptions.")
       FirebaseApp.configure()
       launchOptions?.forEach({ (key: UIApplication.LaunchOptionsKey, value: Any) in
           print(key.rawValue, value)
--- iosApp/iosApp/ui/AppView.swift
+++ iosApp/iosApp/ui/AppView.swift
@@ -7,19 +7,45 @@
 //
 
 import SwiftUI
+import core
 
 struct AppView: View {
-    @EnvironmentObject var authObject: MockObservableObject
-
+    @EnvironmentObject var authObject: AuthObject
+    @State var isErrorDisplayed = false
+    
     var body: some View {
-        Group{
-           AnyView(ContentView())
+        ZStack{
+            Color.background
+            
+            VStack{
+                switch authObject.state {
+                case .Connected:
+                    AnyView(ContentView())// room screen
+                case .UserSigned, .EmptyUserData:
+                    ContentView() // join screen
+                case .InvalidAppVersion:
+                    AnyView(ContentView()) // update app screen
+                case .Loading:
+                    ProgressView()
+                default:
+                    ProgressView()
+                }
+            }.alert("error", isPresented: $isErrorDisplayed, actions: {
+                Button("retry") {
+                    authObject.retry()
+                }
+            })
+            .onReceive(authObject.$state) { obj in
+                isErrorDisplayed = obj == .GeneralError
+            }
         }
     }
 }
 
 struct AppView_Previews: PreviewProvider {
    static var previews: some View {
-       AppView().environmentObject(MockObservableObject())
+       AppView()
+           .environmentObject(AuthObject(controller: MockAuthControllerInterface()))
+//           .environmentObject(AuthObject(controller: AuthController(preferences: nil, serverRepository: ServerRepositoryImpl(baseUrl: "luteoos-scrumbet-poc.northeurope.cloudapp.azure.com:8080", sslPrefix: "http://", httpClient: HttpClientProviderKt.getHttpClient()), applicationVersion: nil)))
    }
 }
