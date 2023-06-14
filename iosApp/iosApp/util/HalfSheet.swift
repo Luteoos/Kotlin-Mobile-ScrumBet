a1adabc2f2c6f2ea1d63403f87065f6fda6c93c9
luteoos <mateusz.lutecki.it@gmail.com>
2023-06-14 15:52:56 +0200

    - added `MainScreenUsernameInputSheet.swift` - added `Localizable.strings`

---
+++ iosApp/iosApp/util/HalfSheet.swift
@@ -0,0 +1,38 @@
+//
+//  HalfSheet.swift
+//  iosApp
+//
+//  Created by Mateusz Lutecki on 14/06/2023.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct HalfSheet<Content>: UIViewControllerRepresentable where Content : View {
+
+    private let content: Content
+    
+    @inlinable init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+    
+    func makeUIViewController(context: Context) -> HalfSheetController<Content> {
+        return HalfSheetController(rootView: content)
+    }
+    
+    func updateUIViewController(_: HalfSheetController<Content>, context: Context) {
+    }
+}
+
+class HalfSheetController<Content>: UIHostingController<Content> where Content : View {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let presentation = sheetPresentationController {
+            presentation.detents = [.medium(), .large()]
+            presentation.prefersGrabberVisible = true
+            presentation.largestUndimmedDetentIdentifier = .medium
+        }
+    }
+}
