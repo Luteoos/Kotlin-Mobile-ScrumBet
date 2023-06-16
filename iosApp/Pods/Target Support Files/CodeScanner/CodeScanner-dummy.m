dc7a00b6af8559dae9d8d994c09fccfbb82c7095
luteoos <mateusz.lutecki.it@gmail.com>
2023-06-16 19:16:31 +0200

    - added pod `Luteoos/CodeScanner`

---
+++ iosApp/Pods/Target Support Files/CodeScanner/CodeScanner-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CodeScanner : NSObject
+@end
+@implementation PodsDummy_CodeScanner
+@end
--- iosApp/iosApp/ui/main/MainScreenJoinSheet.swift
+++ iosApp/iosApp/ui/main/MainScreenJoinSheet.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 struct MainScreenJoinSheet: View {
     let joinRoom: (String) -> ()
