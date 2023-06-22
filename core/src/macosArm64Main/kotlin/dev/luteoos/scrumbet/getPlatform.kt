6fe2a00fd28cf51a2d84299698d3609ca9744b4f
luteoos <mateusz.lutecki.it@gmail.com>
2023-06-22 13:34:06 +0200

    - `ktLint` formatting

--- core/src/macosArm64Main/kotlin/dev/luteoos/scrumbet/getPlatform.kt
+++ core/src/macosArm64Main/kotlin/dev/luteoos/scrumbet/getPlatform.kt
@@ -1,9 +1,8 @@
 package dev.luteoos.scrumbet
 
-class OsXPlatform: Platform{
+class OsXPlatform : Platform {
     override val name: String
         get() = "MacOS"
-
 }
 
-actual fun getPlatform(): Platform  = OsXPlatform()
+actual fun getPlatform(): Platform = OsXPlatform()
