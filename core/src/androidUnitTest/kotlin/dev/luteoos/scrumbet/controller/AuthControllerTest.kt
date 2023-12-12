11a000c7cdac78bcc72e092a32ad8b99c0340984
Luteoos <mateusz.lutecki.it@gmail.com>
2023-12-12 10:42:41 +0100

    - added UnitTests to `AuthControllerTest.kt`

--- core/src/androidUnitTest/kotlin/dev/luteoos/scrumbet/controller/AuthControllerTest.kt
+++ core/src/androidUnitTest/kotlin/dev/luteoos/scrumbet/controller/AuthControllerTest.kt
@@ -1,10 +1,16 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
 package dev.luteoos.scrumbet.controller
 
+import dev.luteoos.scrumbet.core.KState
 import dev.luteoos.scrumbet.data.dto.ServerVersion
+import dev.luteoos.scrumbet.data.entity.AppException
+import dev.luteoos.scrumbet.data.state.AuthState
 import dev.luteoos.scrumbet.domain.repository.interfaces.ServerRepository
 import dev.luteoos.scrumbet.preferences.SharedPreferences
 import dev.luteoos.scrumbet.shared.PlatformBuildConfigInterface
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.take
@@ -20,6 +26,7 @@ import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class AuthControllerTest {
 
@@ -44,19 +51,72 @@ class AuthControllerTest {
     }
 
     @Test
-    fun a() = runTest(UnconfinedTestDispatcher()) {
+    fun `User signed in published to state`() = runTest(UnconfinedTestDispatcher()) {
         `when`(sharedPreferences.getUserDataFlow()).thenReturn(MutableStateFlow(dev.luteoos.scrumbet.data.state.UserData("ut", "ut")))
         `when`(serverRepository.getServerVersionFlow()).thenReturn(flowOf(ServerVersion("1")))
 
         val controller = AuthController(sharedPreferences, serverRepository, buildConfig, "1")
 
         val list = controller.getStateFlow().take(2).toList()
-//        runBlocking {
-//            delay(1000)
-//        }
-//        controller.getStateFlow().value
-//        assertTrue { list.last() is }
-        list.get(0)
-        assertEquals("ut", sharedPreferences.getUserDataFlow().value?.userId)
+        assertTrue { list.last() is KState.Success<AuthState> &&
+                (list.last() as KState.Success<AuthState>).value is AuthState.UserSignedIn }
+//        assertEquals("ut", sharedPreferences.getUserDataFlow().value?.userId)
+    }
+
+    @Test
+    fun `Invalid version published to state`() = runTest(UnconfinedTestDispatcher()) {
+        `when`(sharedPreferences.getUserDataFlow()).thenReturn(MutableStateFlow(dev.luteoos.scrumbet.data.state.UserData("ut", "ut")))
+        `when`(serverRepository.getServerVersionFlow()).thenReturn(flowOf(ServerVersion("2")))
+
+        val controller = AuthController(sharedPreferences, serverRepository, buildConfig, "1")
+
+        val list = controller.getStateFlow().take(2).toList()
+        assertTrue { list.last() is KState.Success<AuthState> &&
+                (list.last() as KState.Success<AuthState>).value is AuthState.InvalidVersion }
+    }
+
+    @Test
+    fun `Connected published to state`() = runTest(UnconfinedTestDispatcher()) {
+        `when`(sharedPreferences.getUserDataFlow()).thenReturn(MutableStateFlow(dev.luteoos.scrumbet.data.state.UserData("ut", "ut")))
+        `when`(serverRepository.getServerVersionFlow()).thenReturn(flowOf(ServerVersion("1")))
+
+        val controller = AuthController(sharedPreferences, serverRepository, buildConfig, "1")
+
+        controller.setRoomConnectionId("unitTest")
+
+        val list = controller.getStateFlow().take(2).toList()
+
+        assertTrue { list.last() is KState.Success<AuthState> &&
+                (list.last() as KState.Success<AuthState>).value is AuthState.Connected }
+
+//        controller.retry()
+//        val listRetry = controller.getStateFlow().take(1).toList()
+//        listRetry.get(0)
+
+    }
+
+    @Test
+    fun `General exception published to state`() = runTest(UnconfinedTestDispatcher()) {
+        `when`(sharedPreferences.getUserDataFlow()).thenReturn(MutableStateFlow(dev.luteoos.scrumbet.data.state.UserData("ut", "ut")))
+        `when`(serverRepository.getServerVersionFlow()).thenReturn(flowOf(null))
+
+        val controller = AuthController(sharedPreferences, serverRepository, buildConfig, "1")
+
+        val list = controller.getStateFlow().take(2).toList()
+
+
+        assertTrue { list.last() is KState.Error<AppException> &&
+                (list.last() as KState.Error<AppException>).error is AppException.GeneralException }
+    }
+
+    @Test
+    fun `Room ID is correctly stripped `() = runTest(UnconfinedTestDispatcher()) {
+        `when`(sharedPreferences.getUserDataFlow()).thenReturn(MutableStateFlow(dev.luteoos.scrumbet.data.state.UserData("ut", "ut")))
+        `when`(serverRepository.getServerVersionFlow()).thenReturn(flowOf(null))
+
+        val controller = AuthController(sharedPreferences, serverRepository, buildConfig, "1")
+
+        controller.setRoomConnectionId("toStrip/tobeStripped/unitTest")
+        assertEquals("unitTest", controller.getRoomConnectionId())
     }
 }
